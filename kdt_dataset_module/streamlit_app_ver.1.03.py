7ac3040bf2b6145f3b799956fc60bd625fa26b6e
yulechestnuts <2bamyule@gmail.com>
2025-03-19 12:24:45 +0900

    update csv 17

--- kdt_dataset_module/streamlit_app_ver.1.03.py
+++ kdt_dataset_module/streamlit_app_ver.1.03.py
@@ -918,7 +918,7 @@ def main():
         </style>
     """, unsafe_allow_html=True)
 
-    url = "https://github.com/yulechestnuts/KDT_Dataset/blob/main/result_kdtdata_202501.csv?raw=true" # Define URL here
+    url = "https://github.com/yulechestnuts/KDT_Dataset/blob/main/result_kdtdata_202502.csv?raw=true" # Define URL here
     df = load_data_from_github(url) # Use load_data_from_github
     if df.empty:
         return
